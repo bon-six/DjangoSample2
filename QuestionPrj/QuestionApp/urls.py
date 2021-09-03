75ec006b4ee18da169c20ffa106cd0009f6931ea
bon-netizen <inner.wellbeing.life@gmail.com>
2021-09-03 11:49:36 +0800

    add static and test files

--- QuestionPrj/QuestionApp/urls.py
+++ QuestionPrj/QuestionApp/urls.py
@@ -6,5 +6,6 @@ from .views import HomepageView, QuestionDetailView
 app_name = QuestionappConfig.name
 urlpatterns = [
     path('question/<int:pk>/', QuestionDetailView.as_view(), name='question_detail'),
+    path('/index.html', HomepageView.as_view(), name='home'),
     path('', HomepageView.as_view(), name='home'),
 ]
